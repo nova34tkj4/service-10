c6b830bedde6e3c05f9d92a0ea7cb2a6b9e6daf3
Your Name <you@example.com>
2026-01-15 21:03:40 +0700

    test

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello semuanya")
+    fmt.Fprintf(w, "Hello semuanya, sekarang hari kamis")
 }
 
 func main() {
